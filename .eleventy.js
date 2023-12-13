787262221829414988a7882633d88c266a1d519b
Tereza Beránková <xteryna@seznam.cz>
2023-12-13 11:06:54 +0100

    11ty templates

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("icons");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
